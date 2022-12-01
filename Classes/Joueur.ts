cccc405984bce5b824c9821eb7d139d3e2226b5e
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-01 08:28:41 +0100

    refactor: ajout d'une variable nbMancheGagne dans le joueur :recycle:

--- Classes/Joueur.ts
+++ Classes/Joueur.ts
@@ -2,14 +2,27 @@ import Gobelet from "./Gobelet";
 export default class Joueur {
   private _nom: string;
   private _score: number;
+  private _nbMancheGagne: number;
 
   constructor(nom: string) {
     this._nom = nom;
     this._score = 0;
+    this._nbMancheGagne = 0;
+  }
+
+  get nbMancheGagne(): number {
+    return this._nbMancheGagne;
   }
 
   get nom(): string {
     return this._nom;
+  }
+
+  /**
+   * Permet d'incrémenter le nombre de manche gagné du joueur
+   */
+  public gagne() {
+    this._nbMancheGagne++;
   }
 
   /**
