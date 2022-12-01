5beabe8aef944b75931aa03e84b05546effac8a0
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-01 15:55:26 +0100

    feat: lance plusieurs partie :sparkles:

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import Partie from "./Classes/Partie";
 
-let partie: Partie = new Partie(3);
-partie.initialiserPartie("Robin", "Nathan", "Alexandre");
-partie.lancerPartie();
-partie.afficherGagnant();
+for (let index = 0; index < 10; index++) {
+  let partie: Partie = new Partie(3);
+  partie.initialiserPartie("Robin", "Nathan", "Alexandre");
+  partie.lancerPartie();
+  partie.afficherGagnant();
+}
